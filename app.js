83cfcd7cc40a917da4ee70230e9c43255d1a0ff5
Jadurani Davalos <jadurani.davalos@gmail.com>
2020-09-20 18:37:18 +0800

    working member get query

--- app.js
+++ app.js
@@ -2,8 +2,16 @@
 
 Trello.authorize({
   name: "RAWR",
-  persist: false,
+  // persist: false,
   expiration: "1hour",
-  success: (maw) => console.log(maw, 'DUCCESS'),
+  success: (maw) => {
+    console.log(maw, 'SUCCESS')
+    Trello.members.get(
+      "me",
+      { fields: "username,fullName" },
+      (rawr) => console.log({rawr}),
+      (meow) => console.error({meow})
+    );
+  },
   error: (maw) => console.error(maw, 'NO!')
 });
