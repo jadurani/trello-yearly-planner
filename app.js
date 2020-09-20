24c60ab1510ac960985c31aa7e710d9279811cb1
Jadurani Davalos <jadurani.davalos@gmail.com>
2020-09-20 21:26:14 +0800

    can create org

--- app.js
+++ app.js
@@ -2,13 +2,33 @@
 
 Trello.authorize({
   name: "RAWR",
-  // persist: false,
+  persist: false,
   expiration: "1hour",
+  scope: { read: true, write: true, account: false },
   success: (maw) => {
     console.log(maw, 'SUCCESS')
     Trello.members.get(
       "me",
       { fields: "username,fullName" },
+      (rawr) => console.log({rawr}),
+      (meow) => console.error({meow})
+    );
+
+    Trello.post(
+      "organizations",
+      {
+        displayName: "2114-test-org",
+        desc: "test desc",
+        name: "sample name",
+      },
+      (postOrg) => console.log({postOrg}),
+      (postOrgErr) => console.error({postOrgErr})
+    );
+
+
+    Trello.members.get(
+      "me/organizations",
+      // { fields: "username,fullName" },
       (rawr) => console.log({rawr}),
       (meow) => console.error({meow})
     );
