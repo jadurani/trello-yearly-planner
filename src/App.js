b10207c3bddd54ac7ee2c802f3e35fd4a3d7de30
Jadurani Davalos <jadurani.davalos@gmail.com>
2020-09-26 00:54:48 +0800

    added bg, added prelim board preview

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './bootstrap.css';
 
 function App() {
   return (
-    <div class="row">
+    <div className="row vh-100 p-0 m-0">
       {/* FORMS */}
-      <div class="col-md-6">
+      <div className="col-md-6 p-0">
         {/* STEP 1 - INTRO */}
         <div>
           <h1>Trello Yearly Planner</h1>
@@ -38,78 +38,18 @@ function App() {
       </div>
 
       {/* PREVIEW */}
-      <div class="col-md-6">
+      <div className="section-preview col-md-6 p-0 d-flex flex-column">
         {/* STEP 1 - INTRO */}
-        <div>
-          {/* 1 - container*/}
+        {/* 1 - container*/}
+        <div className="board-preview h-50 w-50 m-auto p-3">
           <div>
-            <div>
-              title here
-            </div>
-            <div>
-              Your boards will appear here.
-            </div>
+            title here
           </div>
-
-          {/* 2 */}
           <div>
-            <div>
-              title here
-            </div>
-            <div>
-              Board item previews
-            </div>
-          </div>
-
-          {/* 3 */}
-          <div>
-            <div>
-              title here
-            </div>
-            <div>
-              board here -- horizontal scrollable
-            </div>
+            Your boards will appear here.
           </div>
         </div>
 
-        {/* STEP 2 - YEAR?? */}
-        <div>
-          {/* preview in 1.1 */}
-          <div>
-            <div>
-              title here
-            </div>
-            <div>
-              Your boards will appear here.
-            </div>
-          </div>
-        </div>
-
-        {/* STEP 3 - Create boards */}
-        <div>
-          {/* preview in 1.2 */}
-          <div>
-            <div>
-              title here
-            </div>
-            <div>
-              Your boards will appear here.
-            </div>
-          </div>
-        </div>
-
-        {/* STEP 4 - Create lists */}
-        <div>
-          {/* preview in 1.3 */}
-          <div>
-            <div>
-              title here
-            </div>
-            <div>
-              board here -- horizontal scrollable
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
