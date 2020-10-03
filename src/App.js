448e9a651678203dcd8615a209740207b0dacffa
Jadurani Davalos <jadurani.davalos@gmail.com>
2020-10-03 22:21:01 +0800

    switch from team page to board

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import './bootstrap.css';
-import { ALL } from 'dns';
 const moment = window.moment;
 // import { auth } from './services/trello-api';
 
 function App() {
 
+  let [activeBoardIdx, setActiveBoardIdx] = useState(-1);
 
   const boards = [];
   const gratBoards = [];
   let quarterNum = 0;
-  let activeBoardIdx = -1;
+  let currentBoardIdx = -1;
 
   const labelList = [
     {
@@ -68,7 +68,7 @@ function App() {
 
     if (weekNum % 13 === 0) {
       quarterNum += 1;
-      activeBoardIdx += 1;
+      currentBoardIdx += 1;
 
       let boardName = `2021Q${quarterNum}`;
       gratBoards.push({
@@ -82,22 +82,22 @@ function App() {
       });
 
       listNames.forEach(listName => {
-        boards[activeBoardIdx].weekNames.push({
+        boards[currentBoardIdx].weekNames.push({
           name: listName,
           cards: []
         })
       });
 
       labelList.forEach(labelName => {
-        boards[activeBoardIdx].weekNames[0].cards.push(labelName);
+        boards[currentBoardIdx].weekNames[0].cards.push(labelName);
       });
     }
 
-    gratBoards[activeBoardIdx].weekNames.push({
+    gratBoards[currentBoardIdx].weekNames.push({
       name: listName,
       cards: []
     });
-    boards[activeBoardIdx].weekNames.push({
+    boards[currentBoardIdx].weekNames.push({
       name: listName,
       cards: []
     });
@@ -111,6 +111,12 @@ function App() {
   console.log(boards);
   console.log(gratBoards);
   console.log(ALL_BOARDS);
+
+  const openBoard = (selectedBoardIdx) => {
+    console.log(selectedBoardIdx);
+    setActiveBoardIdx(selectedBoardIdx);
+  }
+
 
   return (
     <div className="row vh-100 p-0 m-0">
@@ -126,36 +132,101 @@ function App() {
 
       {/* PREVIEW */}
       {/* <div className="section-preview col-md-6 p-0 d-flex flex-column"> */}
-      <div className="section-preview col-12 p-0 d-flex flex-column">
-        {/* STEP 1 - INTRO */}
-        {/* 1 - container*/}
-        {/* <div className="team-preview h-50 w-75 d-flex flex-column m-auto p-3">
-          <div className="team-name">
-            Team Name!!
-          </div>
-          <div className="flex-grow-1 d-flex justify-content-center align-items-center">
-            Your boards will appear here.
-          </div>
-        </div> */}
 
-        {/* 2 - container */}
-        <div className="team-preview h-50 w-75 d-flex flex-column m-auto pr-3">
-          <div className="team-name mt-3 ml-3">
-            2021Life
-          </div>
-          <div className="team-body flex-grow-1 d-flex flex-wrap pt-2 pb-4 u-fancy-scrollbar">
-            {
-              ALL_BOARDS.map((board, index) => (
-                <div key={`board-${index}`} className="board-tile p-2 mt-3 ml-3">
-                  <div className="board-tile-title">
-                    {board.name}
+        <div className="section-preview col-12 p-0 d-flex flex-column">
+          {/* STEP 1 - INTRO */}
+          {/* 1 - container*/}
+          {/* <div className="team-preview h-50 w-75 d-flex flex-column m-auto p-3">
+            <div className="team-name">
+              Team Name!!
+            </div>
+            <div className="flex-grow-1 d-flex justify-content-center align-items-center">
+              Your boards will appear here.
+            </div>
+          </div> */}
+
+          {/* 2 - container */}
+          { activeBoardIdx === -1 &&
+            <div className="team-preview h-50 w-75 d-flex flex-column m-auto pr-3">
+              <div className="team-name mt-3 ml-3">
+                2021Life
+              </div>
+              {
+                <div className="team-body flex-grow-1 d-flex flex-wrap pt-2 pb-4 u-fancy-scrollbar">
+                  {
+                    ALL_BOARDS.map((board, index) => (
+                      <div key={`board-${index}`} className="board-tile p-2 mt-3 ml-3" onClick={() => openBoard(index)}>
+                        <div className="board-tile-title">
+                          {board.name}
+                        </div>
+                      </div>
+                    ))
+                  }
+                </div>
+              }
+            </div>
+          }
+
+
+          {
+            activeBoardIdx > -1  &&
+            <div className="board-preview h-50 w-75 d-flex flex-column m-auto">
+            {/* 3 - container */}
+              <div className="board-name p-3">
+                { ALL_BOARDS[activeBoardIdx].name }
+              </div>
+              <div className="board-body flex-grow-1 d-flex pt-2 pb-4 u-fancy-scrollbar">
+                <div className="list p-2 ml-3 mr-2 d-flex flex-column">
+                  <div className="list-title">
+                    Week 1
+                  </div>
+                  {/* card list container */}
+                  <div className="card-list flex-grow-1 my-2 u-fancy-scrollbar">
+                    <div className="card p-2 mb-2">
+                      Card Item
+                    </div>
+                    <div className="card p-2 mb-2">
+                      Card Item
+                    </div>
+                    <div className="card p-2 mb-2">
+                      Card Item
+                    </div>
+                    <div className="card p-2 mb-2">
+                      Card Item
+                    </div>
+                    <div className="card p-2 mb-2">
+                      Card Item
+                    </div>
+                    <div className="card p-2 mb-2">
+                      Card Item
+                    </div>
                   </div>
                 </div>
-              ))
-            }
-          </div>
-        </div>
-
+                <div className="list p-2 mr-2">
+                  <div className="list-title">
+                    Week 2
+                  </div>
+                  {/* card list container */}
+                  <div className="card-list my-2">
+                    <div className="card p-2">
+                      Card Item
+                    </div>
+                  </div>
+                </div>
+                <div className="list p-2 mr-4 u-fancy-scrollbar">
+                  <div className="list-title">
+                    Week 2
+                  </div>
+                  {/* card list container */}
+                  <div className="card-list my-2">
+                    <div className="card p-2">
+                      Card Item
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          }
       </div>
     </div>
   );
