15fb6134f458eac3250f73b70ffb3749e19ef30e
Jadurani Davalos <jadurani.davalos@gmail.com>
2020-10-03 21:29:30 +0800

    generate seed data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,109 @@
 import React from 'react';
 import './App.css';
 import './bootstrap.css';
-
+const moment = window.moment;
 // import { auth } from './services/trello-api';
 
 function App() {
+
+
+  const boards = [];
+  const gratBoards = [];
+  let quarterNum = 0;
+  let activeBoardIdx = -1;
+
+  const labelList = [
+    {
+      color: "green",
+      name: "Self-Development / Health / Fitness",
+      hexCode: "#61bd4f",
+    },
+    {
+      color: "yellow",
+      name: "Work",
+      hexCode: "#f2d600",
+    },
+    {
+      color: "red",
+      hexCode: "#eb5a46",
+      name: "Self-Development",
+    },
+    {
+      color: "purple",
+      name: "Causes",
+      hexCode: "#c377e0",
+    },
+    {
+      color: "blue",
+      name: "Socials",
+      hexCode: "#0079bf",
+    },
+    {
+      color: "sky",
+      name: "Hobbies",
+      hexCode: "#00c2e0",
+    },
+    {
+      color: "black",
+      name: "Chores",
+      hexCode: "#344563",
+    }
+  ];
+
+  const listNames = [
+    "Labels",
+    "Won't Do",
+    "Backlog",
+    "Ongoing (long-term)",
+    "Doing today",
+    "Doing now"
+  ]
+
+  for (let weekNum = 0; weekNum < 52; weekNum++) {
+    const weekStart = moment('2021').week(weekNum + 1).day(1).format("MMMDD")
+    const weekEnd = moment('2021').week(weekNum + 1).day(7).format("MMMDD")
+    const listName = `${weekStart}-${weekEnd}`
+
+    if (weekNum % 13 === 0) {
+      quarterNum += 1;
+      activeBoardIdx += 1;
+
+      let boardName = `2021Q${quarterNum}`;
+      gratBoards.push({
+        name: `${boardName}-Gratitude`,
+        weekNames: []
+      });
+
+      boards.push({
+        name: boardName,
+        weekNames: []
+      });
+
+      listNames.forEach(listName => {
+        boards[activeBoardIdx].weekNames.push({
+          name: listName,
+          cards: []
+        })
+      });
+
+      labelList.forEach(labelName => {
+        boards[activeBoardIdx].weekNames[0].cards.push(labelName);
+      });
+    }
+
+    gratBoards[activeBoardIdx].weekNames.push({
+      name: listName,
+      cards: []
+    });
+    boards[activeBoardIdx].weekNames.push({
+      name: listName,
+      cards: []
+    });
+  }
+
+  console.log(boards);
+  console.log(gratBoards);
+
   return (
     <div className="row vh-100 p-0 m-0">
       {/* FORMS */}
